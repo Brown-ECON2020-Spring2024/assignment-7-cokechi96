0bae1aaaa1fde9b83c18aefaca5da0cbdac71e34
cokechi96 <chidubem_okechi@brown.edu>
2024-03-14 17:35:43 -0400

    added new tests, updated lock file, created output figure

---
+++ tests/test-main.r
@@ -0,0 +1,32 @@
+library(testdat)
+library(testthat)
+library(data.table)
+library(estimatr)
+
+## Import test-relevant data
+data <- fread("../output/data/gapminder.csv")
+model <- readRDS("../output/models/linear_reg.rds")
+
+#source("../code/run_linear_reg.r")
+## Add unit tests as specified by question
+
+test_that("estimated coefficient for gdpPercap is reasonable", {
+    expect_equal(model$coefficients[[2]], 0.00076, tolerance = 0.001)
+})
+
+test_that("number of observations equal 1704", {
+    expect_equal(nobs(model), 1704)
+})
+
+test_that("year and country create a unique id", {
+    expect_unique(data = data, c(year, country))
+})
+
+test_that("gdpPercap observations are between 0 and 1e6", {
+    expect_range(data = data, gdpPercap, 0, 1e6)
+})
+
+test_that("the continent encompasses the 5 main continents", {
+    expect_values(data = data, continent, c("Asia", "Europe", 
+     "Africa", "Americas", "Oceania"))
+})
